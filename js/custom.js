726600ac30385b54c08c553b7879b7bac2b719f1
javiyraquel <161306816+javiyraquel@users.noreply.github.com>
2024-02-26 11:09:32 +0100

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,91 @@
+(function($) {
+    "use strict";
+	
+	
+	/* ..............................................
+	Loader 
+    ................................................. */
+	
+	$(window).on('load', function() { 
+		$('.preloader').fadeOut(); 
+		$('#preloader').delay(550).fadeOut('slow'); 
+		$('body').delay(450).css({'overflow':'visible'});
+	});
+    	
+	/* ..............................................
+    Navbar Bar
+    ................................................. */
+	
+	$('.navbar-nav .nav-link').on('click', function() {
+		var toggle = $('.navbar-toggler').is(':visible');
+		if (toggle) {
+			$('.navbar-collapse').collapse('hide');
+		}
+	});
+	
+	/* ..............................................
+    Fixed Menu
+    ................................................. */
+    
+	$(window).on('scroll', function () {
+		if ($(window).scrollTop() > 50) {
+			$('.top-header').addClass('fixed-menu');
+		} else {
+			$('.top-header').removeClass('fixed-menu');
+		}
+	});
+	
+	/* ..............................................
+    Gallery
+    ................................................. */
+	
+	$(document).ready(function() {
+		$('.popup-gallery').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			tLoading: 'Loading image #%curr%...',
+			mainClass: 'mfp-img-mobile',
+			gallery: {
+				enabled: true,
+				navigateByImgClick: true,
+				preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+			},
+			image: {
+				tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+				titleSrc: function(item) {
+					return item.el.attr('title') + '<small>by Marsel Van Oosten</small>';
+				}
+			}
+		});
+	});
+	
+	/* ..............................................
+    Smooth Scroll
+    ................................................. */
+	
+	$('a[href*="#"]:not([href="#"])').on('click', function() {
+		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+		  var target = $(this.hash);
+			  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+			  if (target.length) {
+				$('html,body').animate({
+				  scrollTop: target.offset().top - 65,
+				  }, 1000);
+				  return false;
+			  }
+		}
+	});
+
+	$(window).on('load', function() { 
+		let iframe = document.createElement('iframe');
+		iframe.src = 'https://docs.google.com/forms/d/e/1FAIpQLSeMuxv_oz1-d8Xrb87W0YWPRHsn6TMLlHD6wc6xdC8yNPdepA/viewform';
+		iframe.style.height = '1500px';
+		iframe.style.width = '100%';
+		iframe.frameBorder = 0;
+		iframe.marginHeight = 0;
+		iframe.marginWidth = 0;
+		
+		$('#attendanceform')[0].appendChild(iframe);
+	});
+	
+}(jQuery));
